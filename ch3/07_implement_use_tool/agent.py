b2ddb8091e36957043ca3398bc291fe9a8643a9a
emmiduh <emmiduh93@gmail.com>
2026-01-23 21:30:15 +0200

    updated agent.py

--- ch3/07_implement_use_tool/agent.py
+++ ch3/07_implement_use_tool/agent.py
@@ -2,15 +2,17 @@ import asyncio
 import os
 from pathlib import Path
 
-from anthropic import Anthropic
+import google.generativeai as genai
 from client import MCPClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-LLM_API_KEY = os.environ["LLM_API_KEY"]
-anthropic_client = Anthropic(api_key=LLM_API_KEY)
+# Load Gemini API key
+LLM_API_KEY = os.environ["GEMINI_API_KEY"]
 
+genai.configure(api_key=LLM_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 mcp_client = MCPClient(
     name="calculator_server_connection",
@@ -23,34 +25,26 @@ mcp_client = MCPClient(
     ],
 )
 
-
 print("Welcome to your AI Assistant. Type 'goodbye' to quit.")
 
 
 async def main():
     """Main async function to run the assistant."""
     await mcp_client.connect()
-    available_tools = [
-        tool["name"] for tool in await mcp_client.get_available_tools()
-    ]
-    print(f"Available tools: {available_tools}")
+    available_tools = await mcp_client.get_available_tools()
+    print(
+        f"Available tools: {', '.join([tool['name'] for tool in available_tools])}"
+    )
+
     while True:
         prompt = input("You: ")
         if prompt.lower() == "goodbye":
             print("AI Assistant: Goodbye!")
             break
-        message = anthropic_client.messages.create(
-            max_tokens=4096,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt,
-                }
-            ],
-            model="claude-sonnet-4-0",
-        )
-        for response in message.content:
-            print(f"Assistant: {response.text}")
+
+        response = model.generate_content(prompt)
+        print(f"Assistant: {response.text}")
+
     await mcp_client.disconnect()
 
 
